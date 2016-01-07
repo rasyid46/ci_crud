4164f4c0e6c4d643cd41e7c4eac3551d0cc8aa93
rasyid <yuhu@rasyid.com>
2016-01-07 13:42:59 +0700

    CI simple

--- application/views/admin/v_view.php
+++ application/views/admin/v_view.php
@@ -1,12 +1,22 @@
-<table border="1">
-    <tr>
-	<td>No</td>    
-	<td>Nama</td>    
-	<td>User</td>    
-	<td>Password</td>    	
-	<td>edit</td>    	
-	<td>delete</td>    	
-    </tr>
+<html>
+    <head>
+        <title>User</title>
+    </head>
+<body>
+<p>DAFTAR User</p>
+<br> <a href =<?php echo base_url();?>index.php/admin/create/> Tambah Data </a>
+<table width="415" border="1">
+        <thead>
+          <tr>
+            <th width="24">No.</th>
+            <th width="133">Nama</th>
+            <th width="85">Username</th>
+            <th width="56">password</th>
+            <th width="38">Edit</th>
+            <th width="45">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
     <?php
     if($list_user->result()){
 	
@@ -29,4 +39,3 @@ echo "<tr><td colspan=6>Data tidak ada </td></tr>";
     }
     ?>
 </table>
-<br> <a href =<?php echo base_url();?>index.php/admin/create/> Tambah Data </a>
